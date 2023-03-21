207517dfa08d8d3090f7e806c14f176d242d227c
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-03-21 14:43:00 +0000

    AP-3842: handle non-existant/unuathorised users
    If a user does not exist we want to fallback to
    unauthenticated root with a message to that affect.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,12 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  def azure_ad
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user&.persisted?
+    if @user && @user&.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:alert] = @user.errors.full_messages.join("<br>")
-      Rails.logger.error "Couldn't login user: #{@user.errors.inspect}"
-      redirect_back(fallback_location: root_path, allow_other_host: false) # Don't redirect back to Microsoft
+      flash[:alert] = "User not found or authorised!"
+      Rails.logger.error "Couldn't login user"
+      redirect_back(fallback_location: unauthenticated_root_path, allow_other_host: false)
     end
   end
 
