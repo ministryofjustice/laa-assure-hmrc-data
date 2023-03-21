0c6f271f26d421f534e996f2e276248aa47260ad
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-03-21 13:00:24 +0000

    AP-3842: fix specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:email, :first_name, :last_name) }
 
-  describe '#email' do
-    let(:email) { 'Jo.Example@example.com' }
-    let(:user) { described_class.create!(email:) }
+  describe "#email" do
+    let(:email) { "Jo.Example@example.com" }
+    let(:provider) { "azure_ad" }
+    let(:user) { described_class.create!(email:, provider: "azure_ad") }
 
     before { user }
 
-    it 'preserves the case it was created with' do
-      expect(user.email).to eq email
+    it "downcases the email automatically" do
+      expect(user.email).to eq email.downcase
     end
 
-    it 'is case insensitive when queried' do
-      query_email = email.dup.downcase
-      expect(described_class.find_by(email: query_email).email).to eq email
+    it "is case insensitive when queried" do
+      query_email = email
+      expect(described_class.find_by(email: query_email).email).to eq email.downcase
     end
 
-    it 'has case insensitive uniqueness enforced by the db' do
-      expect { described_class.create!(email: email.downcase) }.to(
+    it "has case insensitive uniqueness enforced by the db" do
+      expect { described_class.create!(email: email.downcase, provider: "azure_ad") }.to(
         raise_error(ActiveRecord::RecordNotUnique,
                     /Key \(email\)=\(jo.example@example.com\) already exists/)
       )
