caff7c9cd2d753bad219dec173f725c2d17d9c66
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-04-25 22:06:12 +0100

    Swallow stdout message from devise
    Printed to STDOUT during test suite run
    with no obvious way to switch it off during
    testing.

--- spec/system/support/omniauth_helper.rb
+++ spec/system/support/omniauth_helper.rb
@@ -16,13 +16,30 @@ RSpec.configure do |config|
   end
 end
 
-# Stub omniauth failure and reset back to success afterward
+# Stub omniauth failure and reset back to success afterward.
+# Also, swallow stdout message from devise
+# nameley: "ERROR -- omniauth: (azure_ad) Authentication failure! invalid_credentials encountered"
 RSpec.configure do |config|
-  config.before(:each, omniauth_failure: true) do
-    OmniAuth.config.mock_auth[:azure_ad] = :invalid_credentials
+  config.around(:each, omniauth_failure: true) do |example|
+    mock_azure_ad_invalid_credentials do
+      silence_stdout do
+        example.run
+      end
+    end
   end
+end
 
-  config.after(:each, omniauth_failure: true) do
-    OmniAuth.config.mock_auth[:azure_ad] = MockAzureAdAuthHash::JIM_BOB
-  end
+def mock_azure_ad_invalid_credentials
+  OmniAuth.config.mock_auth[:azure_ad] = :invalid_credentials
+  yield
+ensure
+  OmniAuth.config.mock_auth[:azure_ad] = MockAzureAdAuthHash::JIM_BOB
+end
+
+def silence_stdout
+  original_stdout = $stderr.dup
+  $stdout.reopen("/dev/null", "w")
+  yield
+ensure
+  $stdout.reopen(original_stdout)
 end
