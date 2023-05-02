31e5297ff1082c332dc3f5c5b38cb1426713ce26
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2023-05-02 16:22:00 +0100

    ap-3844: add redis, sidekiq and sidekiq_queue to status endpoint

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,10 +3,13 @@ class StatusController < ApplicationController
 
   def status
     checks = {
-      database: database_alive?
+      database: database_alive?,
+      redis: redis_alive?,
+      sidekiq: sidekiq_alive?,
+      sidekiq_queue: sidekiq_queue_healthy?,
     }
 
-    status = :bad_gateway unless checks.values.all?
+    status = :bad_gateway unless checks.except(:sidekiq_queue).values.all?
     render status:, json: { checks: }
   end
 
@@ -23,6 +26,32 @@ private
   def database_alive?
     ActiveRecord::Base.connection.active?
   rescue PG::ConnectionBad
+    false
+  end
+
+  def redis_alive?
+    Sidekiq.redis(&:info)
+    true
+  rescue StandardError
+    false
+  end
+
+  def sidekiq_alive?
+    ps = Sidekiq::ProcessSet.new
+    # rubocop:disable Style/ZeroLengthPredicate
+    !ps.size.zero?
+    # rubocop:enable Style/ZeroLengthPredicate
+  rescue StandardError
+    false
+  end
+
+  def sidekiq_queue_healthy?
+    dead = Sidekiq::DeadSet.new
+    retries = Sidekiq::RetrySet.new
+    # rubocop:disable Style/ZeroLengthPredicate
+    dead.size.zero? && retries.size.zero?
+    # rubocop:enable Style/ZeroLengthPredicate
+  rescue StandardError
     false
   end
 end
