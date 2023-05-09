4c2dc8c1bf92cf2c281c242ca1a48117b46765ae
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2023-05-09 14:14:33 +0100

    ap-4082: add queue_name_service

---
+++ spec/services/queue_name_service_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe QueueNameService do
+  describe ".call" do
+    subject(:call) { described_class.call }
+
+    context "when the service is called in UAT" do
+      before do
+        Rails.configuration.x.environment = "uat"
+        Rails.configuration.x.status.app_branch = "this-is/a.test-branch"
+      end
+
+      it "prefixes the submission queue name with the branch name" do
+        expect(call).to eq "this-is-a-test-branch-submissions"
+      end
+    end
+
+    context "when the service is called anywhere other than UAT" do
+      before do
+        Rails.configuration.x.environment = "test"
+      end
+
+      it "sets the submission queue name to /submissions/" do
+        expect(call).to eq "submissions"
+      end
+    end
+  end
+end
