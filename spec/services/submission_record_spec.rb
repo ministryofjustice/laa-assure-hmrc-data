338ea435fcea7e344d9d39d1f4605f7e12cfea18
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-05-25 21:44:22 +0100

    AP-4054: extend unit tests for submission_record struct

--- spec/services/submission_record_spec.rb
+++ spec/services/submission_record_spec.rb
@@ -42,17 +42,41 @@ RSpec.describe SubmissionRecord do
     subject(:period_start_at) { instance.period_start_at }
 
     it { is_expected.to be_a(Date) }
+
+    context "when date is not valid" do
+      let(:row) { { period_start_date: "2023-01-32" } }
+
+      it "raises date error" do
+        expect { period_start_at }.to raise_error Date::Error, "invalid date for period_start_at"
+      end
+    end
   end
 
   describe "#period_end_at" do
     subject(:period_end_at) { instance.period_end_at }
 
     it { is_expected.to be_a(Date) }
+
+    context "when date is not valid" do
+      let(:row) { { period_end_date: "2023-01-32" } }
+
+      it "raises date error" do
+        expect { period_end_at }.to raise_error Date::Error, "invalid date for period_end_at"
+      end
+    end
   end
 
   describe "#dob" do
     subject(:dob) { instance.dob }
 
     it { is_expected.to be_a(Date) }
+
+    context "when date is not valid" do
+      let(:row) { { date_of_birth: "2001-01-32" } }
+
+      it "raises date error" do
+        expect { dob }.to raise_error Date::Error, "invalid date for dob"
+      end
+    end
   end
 end
