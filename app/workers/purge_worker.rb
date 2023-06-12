eaa5a65a911d63239c34328c3971b679e6868ce9
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2023-06-12 13:07:04 +0100

    ap-4122: add purge_worker job

---
+++ app/workers/purge_worker.rb
@@ -0,0 +1,15 @@
+class PurgeWorker < ApplicationWorker
+
+  def perform
+    bulk_submissions = BulkSubmission.where(expires_at: ..Time.zone.now)
+    bulk_submissions.each do |bulk_submission|
+      bulk_submission.original_file.purge
+      bulk_submission.result_file.purge
+      bulk_submission.submissions.each do |submission|
+        submission.update!(first_name: 'purged', last_name: 'purged', dob: '01/01/1970', nino: 'AB123456C')
+      end
+    end
+
+    super
+  end
+end
