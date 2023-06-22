931b8c57b8894d75f66182a9563a6968e329c102
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-06-22 22:58:12 +0100

    Add nonce to the single inline script in application.html.erb
    CSP report shows this was violating the script-src CSP policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,13 +13,13 @@ Rails.application.configure do
     policy.script_src  :self, :https
     policy.style_src   :self, :https
     # Specify URI for violation reports
-    policy.report_uri "/csp-violation-report-endpoint"
+    # policy.report_uri "/csp-violation-report-endpoint"
   end
-#
-#  # Generate session nonces for permitted importmap and inline scripts
-#  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#  config.content_security_policy_nonce_directives = %w(script-src)
-#
-# # Report violations without enforcing the policy.
+
+  # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w[script-src]
+
+  # Report violations without enforcing the policy.
   config.content_security_policy_report_only = true
 end
