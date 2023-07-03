f5f71225920425fecc84718ff945ba7622bc35c4
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-03 11:30:19 +0100

    fix Rails/RootPathnameMethods

--- spec/models/bulk_submission_spec.rb
+++ spec/models/bulk_submission_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe BulkSubmission do
   describe "#original_file" do
     subject { instance.original_file }
 
-    let(:file_io) { File.open(Rails.root.join('spec/fixtures/files/basic_bulk_submission.csv')) }
+    let(:file_io) { Rails.root.join('spec/fixtures/files/basic_bulk_submission.csv').open }
 
     it { is_expected.to be_an_instance_of(ActiveStorage::Attached::One) }
 
