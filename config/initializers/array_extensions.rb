3eac854f3b04942174278360f00ad874bc8bf70a
ColinBruce <6757677+colinbruce@users.noreply.github.com>
2026-04-23 07:19:42 +0100

    Split initializer extensions into two
    This avoids the Style/OneClassPerFile rubocop infraction

--- config/initializers/array_extensions.rb
+++ config/initializers/array_extensions.rb
@@ -1,9 +1,5 @@
 Dir[File.expand_path("../../lib/extensions/*.rb", __dir__)].each { |file| require file }
 
-module Rails
-  extend Extensions::RailsModuleExtension
-end
-
 class Array
   include Extensions::ArrayExtension
 end
